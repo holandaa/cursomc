0831d3da028ca6033e1129f999e46664996e7116
holandaa <holandawend@gmail.com>
2021-03-14 17:44:37 -0300

    REST testado

---
+++ src/main/java/com/holanda/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.holanda.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //classe rest que responde pelo endpoint
+@RequestMapping(value = "/categorias") //aqui é endpoint
+public class CategoriaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando!";
+	}
+	
+}

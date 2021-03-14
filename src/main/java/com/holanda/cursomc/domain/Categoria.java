21c4a05c7989a3967f177c12d010358b44e89311
holandaa <holandawend@gmail.com>
2021-03-14 19:11:41 -0300

    Banco de dados H2 e criacao automatica de base de dados

--- src/main/java/com/holanda/cursomc/domain/Categoria.java
+++ src/main/java/com/holanda/cursomc/domain/Categoria.java
@@ -2,10 +2,25 @@ package com.holanda.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	/* criar tabela  no banco "Categoria"
+	   com os campos id e nome automaticamente,
+	   funcionará pelo pacote JPA 
+	   
+	   @Entity e @Id 
+	   IDENTITY - geração automatica dos ID
+	 */
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	

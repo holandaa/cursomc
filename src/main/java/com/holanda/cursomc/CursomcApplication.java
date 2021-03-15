86849e1bd080272f4e19559acdee50845d4f566d
holandaa <holandawend@gmail.com>
2021-03-15 00:02:13 -0300

    Criando operacao de instanciacao

--- src/main/java/com/holanda/cursomc/CursomcApplication.java
+++ src/main/java/com/holanda/cursomc/CursomcApplication.java
@@ -1,13 +1,39 @@
 package com.holanda.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.holanda.cursomc.domain.Categoria;
+import com.holanda.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	/*
+	 1º implementar CommandLineRunner
+	 2º Estanciar os objetos cat1 e cat2;
+	 3º Para Salvar no banco devo criar a dependencia da
+	 "CategoriaRepository", assim os dados serão salvos lá
+	 */
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
 }

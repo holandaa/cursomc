0aa6801b09d9caa35a69b63ead89ace5d955e381
holandaa <holandawend@gmail.com>
2021-03-20 15:00:30 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/holanda/cursomc/resources/ClienteResource.java
@@ -0,0 +1,29 @@
+package com.holanda.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.holanda.cursomc.domain.Cliente;
+import com.holanda.cursomc.services.ClienteService;
+
+@RestController //classe rest que responde pelo endpoint
+@RequestMapping(value="/clientes") //aqui é endpoint
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
